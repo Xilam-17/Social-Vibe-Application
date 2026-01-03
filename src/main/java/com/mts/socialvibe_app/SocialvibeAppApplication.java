064ce2f124e04659c75bb96bdd0ab815e964b94b
Aung Myo Thein <aungmyothein1711@gmail.com>
2026-01-03 17:39:38 +0630

    Project started.

---
+++ src/main/java/com/mts/socialvibe_app/SocialvibeAppApplication.java
@@ -0,0 +1,13 @@
+package com.mts.socialvibe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialvibeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialvibeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mts/socialvibe_app/SocialvibeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.socialvibe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialvibeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
